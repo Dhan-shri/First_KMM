9a2a621a772bb6f651c2f7f93cbe4900a7b77f2d
Dhanshri <dhanshri20@navgurukul.org>
2024-01-15 11:19:55 +0530

    Initial commit

---
+++ shared/src/iosMain/kotlin/com/dhanshri/firstkmm/Platform.ios.kt
@@ -0,0 +1,9 @@
+package com.dhanshri.firstkmm
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
